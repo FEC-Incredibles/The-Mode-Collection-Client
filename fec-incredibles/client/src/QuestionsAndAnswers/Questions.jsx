44f22e789ea7e032304b4a53286f7ff2d8391385
JeffHParker <jeffhartmanparker@gmail.com>
2023-05-05 11:17:09 -0700

    components

--- fec-incredibles/client/src/QuestionsAndAnswers/Questions.jsx
+++ fec-incredibles/client/src/QuestionsAndAnswers/Questions.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Answers from './AnswersList.jsx';
 
 const Questions = ({currentItemID}) => {
 
   return (
     <div>
       <p>this is the Questions section</p>
+      <Answers />
     </div>
   )
 }
