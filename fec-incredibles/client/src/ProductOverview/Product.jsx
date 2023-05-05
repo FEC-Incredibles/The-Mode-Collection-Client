a70f8132533161d0733b6dc3dd7bc409cd794824
Jason <quintanajason4004@gmail.com>
2023-05-05 19:11:24 -0400

    connected the Product component to get the product details from the server from the api

--- fec-incredibles/client/src/ProductOverview/Product.jsx
+++ fec-incredibles/client/src/ProductOverview/Product.jsx
@@ -1,14 +1,40 @@
 import {useState, useEffect} from 'react'
-import Axios from 'axios'
+import axios from 'axios'
 
 const Product = ({currentItemID}) => {
 
+  const [productDetails, setProductDetails] = useState()
+  const [syles, setStyles] = useState()
+
+  useEffect (() => {
+    if (currentItemID) {
+      axios.get(`/products/${currentItemID}`)
+        .then((response) => {
+          setProductDetails(response.data);
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }, [currentItemID])
+
+  if (!productDetails) {
+    return <div> Loading... </div>
+  }
 
   return (
-    <div>
+    <div className='widget' id='Product'>
       <p>this is the Product section</p>
+      <div>
+        <p>this is the product name {productDetails.name}</p>
+        <p>this is the product category {productDetails.category}</p>
+        <p>this is the product slogan {productDetails.slogan}</p>
+        <p>this is the product description {productDetails.description}</p>
+        <p>this is the product price {productDetails.default_price}</p>
+      </div>
     </div>
   )
 }
+
 
 export default Product
