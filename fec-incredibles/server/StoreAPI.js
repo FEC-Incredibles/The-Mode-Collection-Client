c69194c65ac40acbd14c34093938b164040aca40
Jason <quintanajason4004@gmail.com>
2023-05-05 16:15:58 -0400

    created the endpoint to get product specific info

--- fec-incredibles/server/StoreAPI.js
+++ fec-incredibles/server/StoreAPI.js
@@ -1,18 +1,25 @@
 const axios = require('axios');
 const config = require('../config.js');
+const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products'
+
+const options = {
+  headers: {
+    'User-Agent': 'request',
+    'Authorization': config.TOKEN,
+    'contentType': 'application/json'
+  }
+}
 
 module.exports.getProducts = () => {
-  const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products'
-
-  const options = {
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': config.TOKEN,
-      'contentType': 'application/json'
-    }
-  }
 
   return axios.get(url, options)
   .then((response) => { return response; })
-  .catch((err) => { console.log('LOOK HERE => ', err); })
+  .catch((err) => { console.log('API ERROR GETTING PRODUCTS => ', err); })
+}
+
+module.exports.getProductInfo = (productID) => {
+
+  return axios.get(`${url}/${productID}`, options)
+  .then((response) => {return response})
+  .catch((err) => { console.log('API ERROR GETTING PRODUCT INFO FOR ', productID, '=> ', err)})
 }
