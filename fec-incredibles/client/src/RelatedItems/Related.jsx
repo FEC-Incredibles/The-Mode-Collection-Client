5568cbd6cdca427e3b35903e5cd532fdf05e7b9f
Brian Gage <brianlgage@gmail.com>
2023-05-08 10:59:18 -0600

    Revert "Revert "feat: mvp carasaul (I have no idea how to spell that) >'o'<""
    This reverts commit a15836c92e85545585313116b282c25f2bfc3f2a.

--- fec-incredibles/client/src/RelatedItems/Related.jsx
+++ fec-incredibles/client/src/RelatedItems/Related.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card.jsx";
 import relatedProducts from "./ExampleData/relatedProducts.json";
 
 const Related = ({ currentItemID }) => {
+  const [activeItem, setActiveItem] = useState(0);
+
+  const updateItem = (newItem) => {
+    if (newItem < 0) {
+      newItem = 0;
+    } else if (newItem >= relatedProducts.length) {
+      newItem = relatedProducts.length - 1;
+    }
+    setActiveItem(newItem);
+  };
   return (
     <div className="widget" id="relatedProducts">
-      <div className="relatedCarousel" style={{ transform: "translateX(-0%)"}}>
-        <div className="viewPort">
+      <div className="relatedCarousel">
+        <div
+          className="viewPort"
+          style={{ transform: `translateX(-${activeItem * 100}%)` }}
+        >
           {relatedProducts.map((item, index) => {
             return <Card item={item} key={index} width={"100%"} />;
           })}
         </div>
+        <button
+          id="backClick"
+          onClick={() => {
+            updateItem(activeItem - 1);
+          }}
+        >
+          Backward
+        </button>
+        <button
+          id="forwardClick"
+          onClick={() => {
+            updateItem(activeItem + 1);
+          }}
+        >
+          Forward
+        </button>
       </div>
     </div>
   );
