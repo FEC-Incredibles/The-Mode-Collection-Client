8eeeae6a3e192072a4b92d77dedbf96708a6fcf3
Jason <quintanajason4004@gmail.com>
2023-05-09 17:15:49 -0400

    step 1 of fixing the css for the images, right now they are just smaller

--- fec-incredibles/client/src/ProductOverview/ProductImages.jsx
+++ fec-incredibles/client/src/ProductOverview/ProductImages.jsx
@@ -14,22 +14,22 @@ const ProductImages = ({ selectedStyleData }) => {
 
 	return (
 		<div id="imageContainer">
-			{/* main image */}
-			<img src={selectedImage.url ? selectedImage.url : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='}></img>
 
 			{/* additional row of images */}
-			<ul id="extraImages">
-				{selectedStyleData.photos.map((photo, index) => (
-					<li key={index}>
-						<img
-							onClick={(e) => {
-								changeImage(index);
-							}}
-							src={photo.thumbnail_url }
+			{selectedStyleData.photos.map((photo, index) => (
+				<div className="images-row" key={index}>
+					<img
+						className="images-extra"
+						onClick={(e) => {
+							changeImage(index);
+						}}
+						src={photo.thumbnail_url}
 						></img>
-					</li>
-				))}
-			</ul>
+				</div>
+			))}
+
+			{/* main image */}
+			<img src={selectedImage.url ? selectedImage.url : "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="}></img>
 		</div>
 	);
 };
