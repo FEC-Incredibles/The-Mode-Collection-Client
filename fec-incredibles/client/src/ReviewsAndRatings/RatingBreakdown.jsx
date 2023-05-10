d7a3b0713ac97a278a7d2abd06c11003a1ef51cc
Shuhua-L <sl.shuhualiu@gmail.com>
2023-05-10 16:47:34 -0400

    feat: get the reviews metadata of the current product on homepage, and dynamically render the avg rating

--- fec-incredibles/client/src/ReviewsAndRatings/RatingBreakdown.jsx
+++ fec-incredibles/client/src/ReviewsAndRatings/RatingBreakdown.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import StarRating from '../StarRating.jsx';
 import { getAvgRating, starRating } from './helper.js';
 
-const RatingBreakdown = ({ reviewsMeta, numOfReviews }) => {
+const RatingBreakdown = ({ reviewsMeta, numOfReviews, avgRating }) => {
 
   // const avgRating = getAvgRating(reviewsMeta);
-  const avgRating = 3.6;
+  // const avgRating = 3.6;
+
 
   const getPercentRecommended = () => {
     return Math.round((reviewsMeta.recommended.true / numOfReviews) * 100);
@@ -19,13 +20,16 @@ const RatingBreakdown = ({ reviewsMeta, numOfReviews }) => {
   const percentRecommended = getPercentRecommended();
 
 
+
+
+
   return (
     <div className="" id="rating-breakdown">
 
       <div className="breakdown-summary">
         <h1 className="breakdown-heading">{avgRating}</h1>
         {/* {starRating(Math.round(avgRating))} */}
-        <StarRating rating={avgRating}/>
+        <StarRating rating={avgRating} />
       </div>
 
       <br />
@@ -34,7 +38,7 @@ const RatingBreakdown = ({ reviewsMeta, numOfReviews }) => {
       </div>
 
       <br />
-      {Object.keys(reviewsMeta.ratings).reverse().map((rating, idx) => {
+      {numOfReviews > 0 && Object.keys(reviewsMeta.ratings).reverse().map((rating, idx) => {
         return (
           <div className="breakdown-by-star" key={idx} >
             {/* TODO: click on it will filter the displaying reviews */}
