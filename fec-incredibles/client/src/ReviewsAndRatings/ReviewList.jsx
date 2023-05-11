2760ac06705f55f7a4cad39787a326ec476983b8
Shuhua-L <sl.shuhualiu@gmail.com>
2023-05-10 22:20:19 -0400

    feat: clicking the "More Review" button renders two more reviews

--- fec-incredibles/client/src/ReviewsAndRatings/ReviewList.jsx
+++ fec-incredibles/client/src/ReviewsAndRatings/ReviewList.jsx
@@ -1,26 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 import ReviewTile from './ReviewTile.jsx';
 
 const ReviewList = ({ reviews }) => {
 
-const [ createMode, setCreateMode ] = useState(false);
+  const [createMode, setCreateMode] = useState(false);
+  const [moreReview, setMoreReview] = useState(reviews.length >= 2);
+  const [currentDisplay, setCurrentDisplay] = useState(reviews.slice(0, 2));
 
-const toggleCreateMode = () => {
-  setCreateMode(!createMode);
-}
+  // useEffect(() => {
+  //   if (reviews.length === currentDisplay.length) {
+  //     setMoreReview(false);
+  //   }
+  // }, [currentDisplay])
+
+  const toggleCreateMode = () => {
+    setCreateMode(!createMode);
+  }
+
+  const handleClickMore = () => {
+    let currentLength = currentDisplay.length;
+    if (reviews.length > currentLength) {
+      setCurrentDisplay(reviews.slice(0, currentLength + 2));
+      if (reviews.length <= currentLength + 2) {
+        setMoreReview(false);
+      }
+    }
+  }
 
   return (
-    <div className="">
+    <div id="review-list">
 
-      {reviews.map((review, idx) => {
-        return (
-          <ReviewTile review={review} key={idx} />
-        )
-      })}
+      <div className="scrollable-list">
+        {currentDisplay.map((review, idx) => {
+          return (
+            <ReviewTile review={review} key={idx} />
+          )
+        })}
+        {moreReview || (
+          <h6> --- No more reviews --- </h6>
+        )}
+      </div>
 
-      <button> More Review </button>
+      {moreReview && (
+        <button onClick={handleClickMore}> More Review </button>
+      )}
+
       <button onClick={toggleCreateMode} > Add New Review </button>
 
       {createMode && (
@@ -29,9 +55,9 @@ const toggleCreateMode = () => {
             <button onClick={toggleCreateMode}> ❌ </button>
 
 
-              <form className="form-new-review">
+            <form className="form-new-review">
 
-              </form>
+            </form>
 
           </div>
         </div>
