7d1bdd1dcc7cd5812a068ae27b7adeefe71ce5e4
JeffHParker <jeffhartmanparker@gmail.com>
2023-05-11 15:29:28 -0700

    search bar is working dynamically

--- fec-incredibles/client/src/QuestionsAndAnswers/Questions.jsx
+++ fec-incredibles/client/src/QuestionsAndAnswers/Questions.jsx
@@ -6,28 +6,42 @@ import ExampleData from './ExampleData.json';
 
 const Questions = ({currentItemID}) => {
 
-  let [questionList, setQuestionList] = useState([]);
+  const [questionList, setQuestionList] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+
 
   useEffect(() => {
-    axios.get(`/qa/questions/${currentItemID}`)
-    .then((response) => {
-      setQuestionList(response.data)
-    })
-    .catch((err) => {
-      console.log('ERR GETTING QUESTIONS FOR PRODUCT ', err)
-    })
-  }, []);
+    if (currentItemID) {
+      axios.get(`/qa/questions/${currentItemID}`)
+      .then((response) => {
+        console.log('resposne.data.results', response.data);
+        setQuestionList(response.data.results);
+      })
+      .catch((err) => {
+        console.log('ERR GETTING QUESTIONS FOR PRODUCT ', err)
+      })
+    }}, [currentItemID]);
 
+    const handleSearch = (e) => {
+      var value = e.target.value;
+      if (value.length > 2) {
+        questionList.forEach(question => {
+          if (question.question_body.includes(value)) {
+            setQuestionList([question]);
+          }
+        })
+      }
+    }
 
+  //   if (!questionList) {
+  //   return <div>Loading</div>
+  // }
   return (
     <div className='widget' id='QA'>
       <h1 className='pageTitle'>Question & Answer</h1>
-        <input className='searchBar' placeholder='Have A Question? Search For Answers...'></input>
+        <input className='searchBar' placeholder='Have A Question? Search For Answers...' onChange={handleSearch}></input>
       <div>
-        <QuestionEntry questions={ExampleData}/>
-      </div>
-      <div>
-        <button>Load More Answers</button>
+        <QuestionEntry questions={questionList}/>
       </div>
       <div className='buttons'>
         <button className='moreAnsweredBtn'>More Answered Questions</button> <button className='addQuestionBtn'>Add A Question</button>
