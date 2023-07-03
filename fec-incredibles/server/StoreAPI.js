3409dbb62f9e8a44e23eadf805d43aaca6bdf22e
Shuhua-L <sl.shuhualiu@gmail.com>
2023-07-03 19:59:17 -0400

    🔧 build:
    used .env instead of config.js for environment variables

--- fec-incredibles/server/StoreAPI.js
+++ fec-incredibles/server/StoreAPI.js
@@ -1,47 +1,71 @@
-const axios = require('axios');
-const config = require('../config.js');
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe'
+const url = process.env.API_URL;
 
 const options = {
   headers: {
-    'User-Agent': 'request',
-    'Authorization': config.TOKEN,
-    'contentType': 'application/json'
-  }
-}
-
+    "User-Agent": "request",
+    Authorization: process.env.API_TOKEN,
+    contentType: "application/json",
+  },
+};
 
 module.exports.getProducts = () => {
-  return axios.get(`${url}/products?count=50`, options)
-  .then((response) => { return response; })
-  .catch((err) => { console.log('API ERROR GETTING PRODUCTS => ', err); })
-}
+  return axios
+    .get(`${url}/products?count=50`, options)
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.log("API ERROR GETTING PRODUCTS => ", err);
+    });
+};
 
 module.exports.getProductInfo = (productID) => {
-
-  return axios.get(`${url}/products/${productID}`, options)
-  .then((response) => {return response})
-  .catch((err) => { console.log('API ERROR GETTING PRODUCT INFO FOR ', productID, '=> ', err)})
-}
+  return axios
+    .get(`${url}/products/${productID}`, options)
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.log("API ERROR GETTING PRODUCT INFO FOR ", productID, "=> ", err);
+    });
+};
 
 module.exports.getProductStyles = (productID) => {
-  return axios.get(`${url}/products/${productID}/styles`, options)
-  .then((response) => { return response; })
-  .catch((err) => { console.log('API ERROR GETTING PRODUCT STYLES FOR ', productID, '=> ', err)} )
-}
+  return axios
+    .get(`${url}/products/${productID}/styles`, options)
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.log("API ERROR GETTING PRODUCT STYLES FOR ", productID, "=> ", err);
+    });
+};
 
 module.exports.getRelatedProductsID = (productID) => {
-  return axios.get(`${url}/products/${productID}/related`, options)
-  .then((response) => { return response; })
-  .catch((err) => { console.log('API ERROR GETTING PRODUCT STYLES FOR ', productID, '=> ', err)} )
-}
+  return axios
+    .get(`${url}/products/${productID}/related`, options)
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.log("API ERROR GETTING PRODUCT STYLES FOR ", productID, "=> ", err);
+    });
+};
 
 module.exports.getReviewMeta = (productID) => {
-  return axios.get(`${url}/reviews/meta/?product_id=${productID}`, options)
-  .then((response) => { return response; })
-  .catch((err) => { console.log('API ERROR GETTING PRODUCT META FOR ', productID, '=> ', err)} )
-}
+  return axios
+    .get(`${url}/reviews/meta/?product_id=${productID}`, options)
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.log("API ERROR GETTING PRODUCT META FOR ", productID, "=> ", err);
+    });
+};
 
 // module.exports.getProductQuestions = (productID) => {
 //   return axios.get(`${url}/qa/questions/?product_id=${productID}`, options)
@@ -60,30 +84,30 @@ module.exports.questions = (req, res) => {
     method: req.method,
     url: url + req.url,
     data: req.body,
-    headers: options.headers
+    headers: options.headers,
   })
-  .then((response) => {
-    res.json(response.data)
-  })
-  .catch((err) => {
-    console.log('API ERROR FOR QUESTIONS', err)
-  })
-}
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.log("API ERROR FOR QUESTIONS", err);
+    });
+};
 
 module.exports.answers = (req, res) => {
   axios({
     method: req.method,
     url: url + req.url,
     data: req.body,
-    headers: options.headers
+    headers: options.headers,
   })
-  .then((response) => {
-    res.json(response.data)
-  })
-  .catch((err) => {
-    console.log('API ERROR FOR QUESTIONS', err)
-  })
-}
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.log("API ERROR FOR QUESTIONS", err);
+    });
+};
 
 module.exports.reviews = (req, res) => {
   // console.log("method: ", req.method)
@@ -93,12 +117,12 @@ module.exports.reviews = (req, res) => {
     method: req.method,
     url: url + req.url,
     data: req.body,
-    headers: options.headers
+    headers: options.headers,
   })
-  .then((response) => {
-    res.json(response.data)
-  })
-  .catch((error) => {
-    console.log("API ERROR FOR REVIEWS 🤯:", error)
-  })
-}
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log("API ERROR FOR REVIEWS 🤯:", error);
+    });
+};
